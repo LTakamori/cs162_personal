ba2a8f5d5694c367c1ee6c81a87bf3798d29f0db
LTakamori <925951065@qq.com>
2020-12-25 14:46:48 +0800

    HW4 skeleton finished

--- hw4/mm_alloc.c
+++ hw4/mm_alloc.c
@@ -9,24 +9,78 @@
 #include <stdio.h>
 #include <string.h>
 
+void* global_base = NULL;
+
+meta find_free_block(meta base, size_t size)
+{
+  meta res = NULL;
+  if (base == NULL) {
+    return res;
+  } else {
+    while(base != NULL) {
+      if (base->size >= size) {
+        res = base;
+        break;
+      } else {
+        base = base->next;
+      }
+    }
+  }
+  return res + 1;
+}
+
+meta find_tail(meta base) {
+  while (base != NULL) {
+    base = base->next;
+  }
+  return base;
+}
+
+meta request_block(size_t size) {
+  meta tail = find_tail(global_base);
+  meta block = sbrk(0);
+  sbrk(size + sizeof(struct meta_data));
+  tail->next = block;
+  block->next = NULL;
+  block->size = size;
+  block->free = 0;
+  block->prev = tail;
+  return block + 1;
+}
 
 void* mm_malloc(size_t size)
 {
   //TODO: Implement malloc
-
+  void* res = find_free_block(global_base, size);
+  if (res != NULL) return res;
+  else {
+    res = request_block(size);
+    return res;
+  }
   return NULL;
+}
+
+meta get_meta_block(void* position) {
+  return (meta)position - 1;
+}
+
+void* get_real_block(meta meta_position) {
+  return meta_position + 1;
 }
 
 void* mm_realloc(void* ptr, size_t size)
 {
   //TODO: Implement realloc
-
+  void* dest = mm_malloc(size);
+  void* src = get_meta_block(ptr);
+  memcpy(dest, src, size + sizeof(struct meta_data));
+  mm_free(ptr);
   return NULL;
 }
 
 void mm_free(void* ptr)
 {
   //TODO: Implement free
-
-  return NULL;
+  meta meta_position = get_meta_block(ptr);
+  meta_position -> free = 1;
 }
--- hw4/mm_alloc.h
+++ hw4/mm_alloc.h
@@ -11,8 +11,25 @@
 
 #include <stdlib.h>
 
+extern void* global_base; // How to ensure every process have unique global block and tail node?
+
+struct meta_data
+{
+    size_t size;
+    size_t free;
+    struct meta_data *next;
+    struct meta_data *prev;
+};
+
+typedef struct meta_data* meta;
+
 void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
+meta find_free_block(meta base, size_t size);
+meta find_tail(meta base);
+meta request_block(size_t size);
+meta get_meta_block(void* position);
+void* get_real_block(meta meta_position);
 
 #endif
